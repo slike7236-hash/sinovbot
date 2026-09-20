dfe5ccbaf0b2558577ce9d57b4b005ac30ef690d
slike7236-hash <slike7236@gmail.com>
2026-09-20 22:44:32 +0500

    Add new premium user ID to premiumUsers object

--- users.js
+++ users.js
@@ -1,6 +1,7 @@
 // users.js fayli
 const premiumUsers = {
     "651936747": ["ess1", "ess2", "ess3", "ess4", "ess5", "ess6"],
+    "1219146340": ["ess1", "ess2", "ess3", "ess4", "ess5", "ess6"],
     "7363384471": ["ess1", "ess2", "ess3", "ess4", "ess5", "ess6"]
     // Yangi sotib olgan xaridorlarni faqat shu yerga qo'shasiz!
 };
