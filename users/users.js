6d2cbb46ca43624318c34c4b3fa29253b181c963
slike7236-hash <slike7236@gmail.com>
2026-09-20 22:26:56 +0500

    Create users.js with premium user data structure
    Added a premiumUsers object to manage premium user data.

---
+++ users/users.js
@@ -0,0 +1,6 @@
+// users.js fayli
+const premiumUsers = {
+    "651936747": ["ess1", "ess2", "ess3", "ess4", "ess5", "ess6"],
+    "7363384471": ["ess1", "ess2", "ess3", "ess4", "ess5", "ess6"]
+    // Yangi sotib olgan xaridorlarni faqat shu yerga qo'shasiz!
+};
